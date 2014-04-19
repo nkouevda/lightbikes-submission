95b302d4b49fb010799dc6432d4448a33b72a16b
Nikita Kouevda <nkouevda@gmail.com>
2014-04-19 14:06:20 -0700

    Maximize next step.

--- entry.js
+++ entry.js
@@ -3,19 +3,45 @@
 
 Bot.register('nkouevda', function(board_state, player_state, move) {
   var me = board_state.me;
-  var them = board_state.them;
   var board = board_state.board;
   var safe_dirs = board.safe_directions(me);
 
-  if (_.contains(safe_dirs, me.left())) {
-    move(me.left());
-  } else if(_.contains(safe_dirs, me.straight())) {
-    move(me.straight());
-  } else if(_.contains(safe_dirs, me.right())) {
-    move(me.right());
-  } else if(_.contains(safe_dirs, me.sharp_right())) {
-    move(me.sharp_right());
-  } else {
-    move(me.sharp_left());
+  var ordered_safe_dirs = [];
+
+  if (_.contains(safe_dirs, me.sharp_left())) {
+    ordered_safe_dirs.push(me.sharp_left());
   }
+
+  if (_.contains(safe_dirs, me.left())) {
+    ordered_safe_dirs.push(me.left());
+  }
+
+  if (_.contains(safe_dirs, me.straight())) {
+    ordered_safe_dirs.push(me.straight());
+  }
+
+  if (_.contains(safe_dirs, me.right())) {
+    ordered_safe_dirs.push(me.right());
+  }
+
+  if (_.contains(safe_dirs, me.sharp_right())) {
+    ordered_safe_dirs.push(me.sharp_right());
+  }
+
+  var dest = board.new_coords_from_dir(me, ordered_safe_dirs[0]);
+  var best_dir = ordered_safe_dirs[0];
+  var best_num_opts = board.safe_surrounding_tiles(dest).length;
+  var num_opts;
+
+  for (var i = 1; i < ordered_safe_dirs.length; ++i) {
+    dest = board.new_coords_from_dir(me, ordered_safe_dirs[i]);
+    num_opts = board.safe_surrounding_tiles(dest).length;
+
+    if (num_opts > best_num_opts) {
+      num_opts = best_num_opts;
+      best_dir = ordered_safe_dirs[i];
+    }
+  }
+
+  move(best_dir);
 })
