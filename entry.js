4de8d9779489c6b3c63472758ed38c7ce19fe8ba
Nikita Kouevda <nkouevda@gmail.com>
2014-04-19 12:36:19 -0700

    Rename variables; remove trailing semicolon.

--- entry.js
+++ entry.js
@@ -1,8 +1,8 @@
 // Nikita Kouevda
 // 2014/04/19
 
-Bot.register('nkouevda', function(game_state, my_state, done) {
-  var safe_dirs = game_state.board.safe_directions(game_state.me);
+Bot.register('nkouevda', function(board_state, player_state, move) {
+  var safe_dirs = board_state.board.safe_directions(board_state.me);
 
-  done(safe_dirs[Math.floor(Math.random() * safe_dirs.length)]);
-});
+  move(safe_dirs[Math.floor(Math.random() * safe_dirs.length)]);
+})
